b62017268744736cfcbf764fa81379ed26417351
busfamui01188-cloud <bus.famui01188@gmail.com>
2025-08-28 13:56:57 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let shake = 0
+input.onGesture(Gesture.Shake, function () {
+    shake = randint(0, 2)
+    if (shake == 0) {
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            # # # # #
+            . . . # #
+            . . . # #
+            `)
+    } else if (shake == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # # # .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
